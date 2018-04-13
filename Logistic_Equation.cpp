4a84b72afa58015bfe7ac6f75ddb7816a437be50
LazzerDrummer <lazerdrummer@gmail.com>
2018-04-13 23:12:37 +0300

    Can't seem to find a 'r' that gives me randomness.

--- Logistic_Equation.cpp
+++ Logistic_Equation.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
+
+int iteration = 998;
+double x0 = 0.02;
+double r = 3.98934;
+int v[11];
+
+
+int logistic_equation(double p0, double r, int iteration, int count, double range)
+{
+	if (iteration == 0) return count;
+	iteration--;
+	double p;
+	p = r * p0*(1 - p0);
+	if (p < range + 0.1 && p > range) count++;
+	//cout << p << endl;
+	//cout << count << endl;
+	logistic_equation(p, r, iteration, count, range);
+}
+
+
 int main()
 {
-	cout << "ur mamaie olaie";
+	for (double i = 0; i < 10; i++)
+		cout << logistic_equation(x0, r, iteration, 0, i/10) << endl;
 	cin.get();
 }
