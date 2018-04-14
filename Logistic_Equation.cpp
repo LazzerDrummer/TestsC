21bdd95c538167b2e3b8fa5a5e1bac15b1c0b583
LazzerDrummer <lazerdrummer@gmail.com>
2018-04-14 19:52:43 +0300

    Simplified drastically.

--- Logistic_Equation.cpp
+++ Logistic_Equation.cpp
@@ -3,28 +3,31 @@
 using namespace std;
 
 
-int iteration = 998;
-double x0 = 0.02;
-double r = 3.98934;
+int iteration = 99800;
+double x0 = 0.7;
+double r = 3.97;
 int v[11];
 
 
-int logistic_equation(double p0, double r, int iteration, int count, double range)
+void logistic_equation(double p0, double r, int iteration)
 {
-	if (iteration == 0) return count;
-	iteration--;
 	double p;
-	p = r * p0*(1 - p0);
-	if (p < range + 0.1 && p > range) count++;
+	int index;
+	for(int i = iteration;i>0;i--)
+	{
+		p0 = r * p0*(1 - p0);
+		index = p0 * 10;
+		v[index]++;
+	}
 	//cout << p << endl;
 	//cout << count << endl;
-	logistic_equation(p, r, iteration, count, range);
 }
 
 
 int main()
 {
-	for (double i = 0; i < 10; i++)
-		cout << logistic_equation(x0, r, iteration, 0, i/10) << endl;
+	logistic_equation(x0, r, iteration);
+	for(int i = 0; i < 10; i++)
+		cout << v[i] << endl;
 	cin.get();
 }
