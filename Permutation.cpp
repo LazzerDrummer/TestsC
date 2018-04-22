830d809f5f1ef326e5ae2e67e850fe60deda2eef
LazzerDrummer <lazerdrummer@gmail.com>
2018-04-22 23:53:17 +0300

    Equivalent of the permutations.py file.

---
+++ Permutation.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+int v[100];
+
+
+void print_array(int n)
+{
+	for (int i = 1; i <= n; i++)
+		cout << v[i];
+	cout << endl;
+}
+
+void swap(int &a, int &b)
+{
+	int aux = a;
+	a = b;
+	b = aux;
+}
+
+void permutate(int start, int finish)
+{
+	if (start == finish)
+	{
+		print_array(finish);
+		return;
+	}
+	for (int i = start; i <= finish; i++)
+	{
+		swap(v[start], v[i]);
+		permutate(start + 1, finish);
+		swap(v[start], v[i]);
+	}
+}
+
+int main()
+{
+	int n; 
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> v[i];
+	permutate(1, 3);
+	cin.ignore();
+	cin.get();
+}
