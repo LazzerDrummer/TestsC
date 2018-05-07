428c236386858ae3280b350c5499758f2ab812a4
LazzerDrummer <lazerdrummer@gmail.com>
2018-05-08 00:12:44 +0300

    permutations are now sorted and you can use a condition.
    only fo numbas tho

--- Permutation.cpp
+++ Permutation.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
-
+#include <algorithm>
+#include <sstream>
 using namespace std;
 int v[100];
+string s[100000];
+//char z[1000];
+int len = 0;
 
-
-void print_array(int n)
+void save_array(int n)
 {
+	len++;
+	int number = 0;
+	//char str[1000];
 	for (int i = 1; i <= n; i++)
-		cout << v[i];
-	cout << endl;
+		number = number * 10 + v[i];
+		//str[i] = z[v[i]];
+	stringstream ss;
+	ss << number;
+	string str = ss.str();
+	s[len] = str;
+		
 }
 
 void swap(int &a, int &b)
@@ -18,11 +29,28 @@ void swap(int &a, int &b)
 	b = aux;
 }
 
+bool condition(int v[100], int n, int number1, int number2)
+{
+	int index1 = 0;
+	int index2 = 0;
+	int i = 1;
+	while (index1 == 0 && index2 == 0)
+	{
+		if (i > n) return false;
+		if (v[i] == number1)index1 = i;
+		if (v[i] == number2)index2 = i;
+		i++;
+	}
+	if (index1 > index2) return true;
+	else return false;
+}
+
 void permutate(int start, int finish)
 {
 	if (start == finish)
 	{
-		print_array(finish);
+		if (condition(v, finish, 2, 3)) save_array(finish);
+		//save_array(finish);
 		return;
 	}
 	for (int i = start; i <= finish; i++)
@@ -38,8 +66,15 @@ int main()
 	int n; 
 	cin >> n;
 	for (int i = 1; i <= n; i++)
-		cin >> v[i];
+			v[i] = i;
+	//cin.get(z, 1001);
 	permutate(1, n);
+	sort(s, s + len + 1);
+	for (int i = 1; i <= len; i++)
+	{
+		cout << s[i];
+		cout << '\n';
+	}	
 	cin.ignore();
 	cin.get();
 }
