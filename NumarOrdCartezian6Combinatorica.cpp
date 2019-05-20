b182f892963a31b8f4736dd598a7ed906eb60e0b
LazzerDrummer <lazerdrummer@gmail.com>
2019-05-20 19:36:06 +0300

    Added the a) part of this exercise.

--- NumarOrdCartezian6Combinatorica.cpp
+++ NumarOrdCartezian6Combinatorica.cpp
@@ -2,14 +2,16 @@
 #include <fstream>
 using namespace std;
 int NrOrd(int *q, int *m, int **r, int n);
+void Gen(int *q, int *m, int **p, int n, int mx, int k, int *a);
+int ord = -1;
 int main()
 {
-	ifstream f("nrord.txt"); int n,n1;
+	ifstream f("nrord.txt"); int n,n1; bool b;
 	f >> n;
 	int **p = new int*[n];
 	int **r = new int*[n];
 	int *m = new int[n];
-	int *q = new int[n];
+	int q[100] = {0};
 	for (int i = 0; i < n; i++)
 	{
 		f >> m[i];
@@ -24,12 +26,27 @@ int main()
 			r[i][p[i][j]] = j;
 		}
 	}
+	f >> b;
 	f >> n1;
-	for (int i = 0; i < n1; i++)
+	if (!b)
 	{
-		for (int j = 0; j < n; j++)
-			f >> q[j];
-		cout << NrOrd(q, m, r, n) << '\n';
+		int j; int mx=0;
+		for (int i = 0; i < n1; i++)
+		{
+			f >> j; q[j]++;
+			if (j > mx) mx = j;
+		}
+		int *a = new int[n];
+		Gen(q, m, p, n, mx, 0, a);
+	}
+	else 
+	{
+		for (int i = 0; i < n1; i++)
+		{
+			for (int j = 0; j < n; j++)
+				f >> q[j];
+			cout << NrOrd(q, m, r, n) << '\n';
+		}
 	}
 	for (int i = 0; i < n; i++)
 	{
@@ -42,6 +59,28 @@ int main()
 	delete[] q;
 	cin.ignore();
 	cin.get(); return 0;
+}
+
+void Gen(int *q, int *m, int **p, int n, int mx, int k, int *a)
+{
+	if (k == n)
+	{
+		if (q[++ord])
+		{
+			for (int i = 0; i < n; i++)
+			{
+				cout << a[i] << ' ';
+			}
+			cout << '\n';
+		}
+		return;
+	}
+	for (int i = 0; i < m[k]; i++)
+	{
+		a[k] = p[k][i];
+		Gen(q, m, p, n, mx, k + 1, a);
+		if (ord == mx) return;
+	}
 }
 
 int NrOrd(int *q, int *m, int **r, int n)
